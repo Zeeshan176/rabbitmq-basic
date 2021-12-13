4fe62ed09641951515397f71070e0f5763207890
Zeeshan176 <zeeshanmd956@gmail.com>
2021-12-13 13:53:48 +0530

    done Basic RabbitMQ

---
+++ new_task.js
@@ -0,0 +1,26 @@
+let amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err, connection) => {
+    if (err) {
+        throw err;
+    }
+    connection.createChannel((err, channel) => {
+        if (err) {
+            throw err;
+        }
+        let queue = 'task_queue';
+        let msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.sendToQueue(queue, Buffer.from(msg), {
+            persistent: true
+        });
+        console.log(" [x] sent '%s'", msg);
+    });
+    setTimeout(() => {
+        connection.close();
+        process.exit(0);
+    }, 1000);
+})
